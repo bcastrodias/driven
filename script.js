aee47c33fd318cfedac6de103c405960f6b2c1f2
Bruno <b.castro.doute@gmail.com>
2022-06-13 16:01:08 -0300

    bordassincronizadas

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let pegarfrango;
-let pegarcoca;
-let pegarpudim;
 
 function selecionarfrango(elemento) {
-    pegarfrago = elemento.innerHTML;
+   
     const comidacheck = document.querySelector (".comidalinha .borda")
 
     const check = document.querySelector (".comidalinha .ok")
@@ -18,46 +15,46 @@ elemento.classList.add("borda");
 if (check !== null){
     check.classList.remove("ok")
 }
-elemento.querySelector(".certinho").classList.add("ok");
+elemento.querySelector(".verificado").classList.add("ok");
 
-botao()
+
+console.log(elemento)
 }
 
 function selecionarcoca(elemento){
-    pegarcoca = elemento.innerHTML;
-    const cocacheck = document.querySelector (".cocalinha .borda")
-    const check = document.querySelector(".cocalinha .ok ")
+    
+    const cocacheck = document.querySelector (".bebidalinha .borda")
+    const check = document.querySelector(".bebidalinha .ok ")
+console.log(elemento)
 
 if (cocacheck !==null) {
-    cocacheck.classlist.remove ("borda")
+    cocacheck.classList.remove ("borda")
 }
 elemento.classList.add("borda")
 
 if (check !== null){
     check.classList.remove ("ok")
 }
-elemento.querySelector (".certinho").classList.add("ok");
+elemento.querySelector (".verificado").classList.add("ok");
 
-botao()
+
 
 }
 
-function selecionarcoca(elemento){
+function selecionarpudim(elemento){
     pegarpudim = elemento.innerHTML;
     const pudimcheck = document.querySelector (".sobremesalinha .borda")
     const check = document.querySelector(".sobremesalinha .ok ")
 
 if (pudimcheck !==null) {
-    pudimcheck.classlist.remove ("borda")
+    pudimcheck.classList.remove ("borda")
 }
 elemento.classList.add("borda")
 
 if (check !== null){
     check.classList.remove ("ok")
 }
-elemento.querySelector (".certinho").classList.add("ok");
-
-botao()
+elemento.querySelector (".verificado").classList.add("ok");
 
 }
 
@@ -68,7 +65,45 @@ function linkwhats(){
     let confirmapudim = document.querySelector('.sobremesalinha .borda');
 
     if(confirmafrango && confirmacoca && confirmapudim){
+
+        const frangobox = confirmafrango.querySelector('.item')
+        const cocabox = confirmacoca.querySelector('.item')
+        const pudimbox  = confirmapudim.querySelector('.item')
+
+        const frangoprecobox = confirmafrango.querySelector('.preco')
+        const cocaprecobox = confirmacoca.querySelector ('.preco')
+        const pudimprecobox = confirmapudim.querySelector ('.preco')
   
+        const frango = frangobox.innerText
+        const coca = cocabox.innerText
+        const pudim = pudimbox.innerText
+
+        const frangoprecotxt = frangoprecobox.innerText
+        const cocaprecotxt = cocaprecobox.innerText
+        const pudimprecotxt = pudimprecobox.innerText
+
+        const frangopreco = parseFloat(frangoprecotxt)
+         const cocapreco = parseFloat(cocaprecotxt)
+         const pudimpreco = parseFloat(pudimpreco)
+
+         const total = frangopreco + cocapreco + pudimpreco
 
 
+
+         let textodecompra = `
+         Olá, gostaria de fazer o pedido:
+         - Prato: ${frango}
+          - Bebida: ${coca}
+         - Sobremesa: ${pudim}
+         Total: R$ ${total.toFixed(2)}`
+
+          const link = textodecompra;
+         const encodedlink = encodeURIComponent(link, "_blank");
+         const allLink = "https://wa.me/5547997084660?text=" + `${encodedlink}`
+
+         openLink(allLink) }
+        }
+
+function openLink (element) {window.open(element);
+}
 
