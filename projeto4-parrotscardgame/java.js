4b78a2f9351cdab05daeb28d26a2c708436a7fb2
Bruno <b.castro.doute@gmail.com>
2022-06-19 03:12:53 -0300

    projeto4

--- projeto4-parrotscardgame/java.js
+++ projeto4-parrotscardgame/java.js
@@ -9,8 +9,45 @@ GameStart();
 
 //Array de Parrots
 
-parrots = [``
-
-
-
+parrots = ['bobrossparrot',
+'explodyparrot',
+'fiestaparrot',
+'metalparrot',
+'revertitparrot',
+'tripletsparrot',
+'unicornparrot'
 ]
+
+//Pareamento
+
+function EvenCards(cartas){
+    let array = []
+    for(let i = 0; i<cartas; i<array.length){
+        array.push(parrots[i]);
+        array.push(parrots[i]);
+        i++
+}
+    }
+
+//Fator aleatorizante
+
+function rng(){
+    Math.random() - 0.5;
+}
+
+array.sort(rng)
+        
+
+//Criar cartas
+function cardCreate(cartas){
+ document.querySelector("fundo").innerHTML = "";
+   for (let p = 0; p < cards.length
+
+   
+    const newParrot = `
+    <div class="card"> <img src="${p}.gif"/>
+    
+    </div>
+    `
+
+}
